3116c4d0a045ae810b0fc842aaea9268b7981b4b
Roberto Hernandez <00162317@uca.edu.sv>
2021-03-09 15:49:01 -0600

    Primer commit

---
+++ Prueba2Application/src/main/java/com/Shop/Prueba2/JPAConfiguration.java
@@ -0,0 +1,140 @@
+package com.Shop.Prueba2;
+/*
+import java.util.Properties;
+
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class JPAConfiguration {
+	
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPersistenceUnitName("Prueba2");
+		em.setPackagesToScan("com.Shop.Prueba2.domain");
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(hibernateProperties());
+
+		return em;	
+	}
+	
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/Shop");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("root");
+		
+		return dataSource;
+	}
+	
+	Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.enable_lazy_load_no_trans","true");
+		return properties;
+	}
+
+}
+*/
+import java.util.Properties;
+
+import javax.sql.DataSource;
+ 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+ 
+@SpringBootApplication
+ 
+@EnableAutoConfiguration(exclude = { //  
+        DataSourceAutoConfiguration.class, //
+        DataSourceTransactionManagerAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class })
+ 
+public class JPAConfiguration {
+ 
+    @Autowired
+    private Environment env;
+     
+     
+    public static void main(String[] args) {
+        SpringApplication.run(JPAConfiguration.class, args);
+    }
+     
+     
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+ 
+        // See: application.properties
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+ 
+        System.out.println("## getDataSource: " + dataSource);
+ 
+        return dataSource;
+    }
+ 
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        Properties properties = new Properties();
+ 
+        // See: application.properties  
+        properties.put("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"));
+        properties.put("current_session_context_class", //
+                env.getProperty("spring.jpa.properties.hibernate.current_session_context_class"));
+ 
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+ 
+        // Package contain entity classes
+        factoryBean.setPackagesToScan(new String[] { "" });
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.afterPropertiesSet();
+        //
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
+    }
+ 
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+ 
+        return transactionManager;
+    }
+}
+
---
+++ Prueba2Application/src/main/java/com/Shop/Prueba2/dao/AccountDAO.java
@@ -0,0 +1,23 @@
+package com.Shop.Prueba2.dao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.Shop.Prueba2.domain.Account;
+ 
+@Transactional
+@Repository
+public class AccountDAO {
+ 
+    @Autowired
+    private SessionFactory sessionFactory;
+ 
+    public Account findAccount(String userName) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(Account.class, userName);
+    }
+ 
+}
